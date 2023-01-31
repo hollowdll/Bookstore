8883db9b2be0bf03a273e3e62707d1808c4abf7f
hollowdll <pixerchat@gmail.com>
2023-01-31 10:43:12 +0200

    add booklist and index views

--- src/main/java/com/example/app/web/BookController.java
+++ src/main/java/com/example/app/web/BookController.java
@@ -1,16 +1,26 @@
 package com.example.app.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.app.domain.BookRepository;
 
 @Controller
 public class BookController {
+	@Autowired
+	private BookRepository repository; 
 
-	@GetMapping("/index")
-	@ResponseBody
-	public String index() {
-		return "This is index page";
+	@GetMapping(value = {"/", "/index"})
+	public String indexPage() {
+		return "index";
+	}
+	
+	@GetMapping("/booklist")
+	public String showBooklist(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 	
 }

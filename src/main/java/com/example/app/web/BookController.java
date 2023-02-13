d5d66270bd801fd71c8111a9cdbbe08da634c45e
hollowdll <pixerchat@gmail.com>
2023-02-13 13:58:59 +0200

    add REST GET for books

--- src/main/java/com/example/app/web/BookController.java
+++ src/main/java/com/example/app/web/BookController.java
@@ -1,5 +1,6 @@
 package com.example.app.web;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.app.domain.Book;
 import com.example.app.domain.BookRepository;
@@ -76,5 +78,17 @@ public class BookController {
 
 		return "redirect:/booklist";
 	}
+	
+	// REST get all
+	@GetMapping("/api/books")
+	public @ResponseBody List<Book> getAllBooks() {
+		return (List<Book>) bookRepository.findAll();
+	}
+	
+	// REST get by id
+	@GetMapping(value="/api/book/{id}")
+    public @ResponseBody Optional<Book> getBookById(@PathVariable("id") Long bookId) {	
+    	return bookRepository.findById(bookId);
+    } 
 
 }

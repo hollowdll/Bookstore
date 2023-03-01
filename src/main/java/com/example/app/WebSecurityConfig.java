99768931c44fa6ce711a64911439281e1aacc8fe
hollowdll <pixerchat@gmail.com>
2023-03-01 11:57:05 +0200

    change security to use user entities and add BCrypt password hashing

--- src/main/java/com/example/app/WebSecurityConfig.java
+++ src/main/java/com/example/app/WebSecurityConfig.java
@@ -1,25 +1,25 @@
 package com.example.app;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.app.web.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
+	
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+	
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http
@@ -28,6 +28,10 @@ public class WebSecurityConfig {
 				.permitAll()
 				.anyRequest()
 				.authenticated()
+				.and()
+			.headers()		// H2 console
+				.frameOptions()
+				.disable()
 				.and()
 			.formLogin()
 				.defaultSuccessUrl("/booklist", true)
@@ -39,29 +43,14 @@ public class WebSecurityConfig {
 			.httpBasic();
 		return http.build();
 	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		List<UserDetails> users = new ArrayList<UserDetails>();
-
-		PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-		UserDetails user = User
-			.withUsername("user")
-			.password(passwordEncoder.encode("user"))
-			.roles("USER")
-			.build();
-
-		users.add(user);
-
-		user = User
-			.withUsername("admin")
-			.password(passwordEncoder.encode("admin"))
-			.roles("USER", "ADMIN")
-			.build();
-
-		users.add(user);
-
-		return new InMemoryUserDetailsManager(users);
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.userDetailsService(userDetailsService)
+			.passwordEncoder(
+				new BCryptPasswordEncoder(20)
+			);
 	}
+	
 }
